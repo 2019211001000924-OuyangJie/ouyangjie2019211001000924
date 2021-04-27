6b8687b27fcb7dc894885170b513f9172a5232a0
合金装备布狼牙 <1667914879@qq.com>
2021-04-27 16:23:18 +0800

    Home Week8 Commit

---
+++ src/main/java/com/ouyangjie/controller/UpdateUserServlet.java
@@ -0,0 +1,67 @@
+package com.ouyangjie.controller;
+
+import com.ouyangjie.dao.UserDao;
+import com.ouyangjie.model.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Date;
+
+
+
+@WebServlet(name = "UpdateUserServlet", value = "/updateUser")
+public class UpdateUserServlet extends HttpServlet {
+    Connection con=null;
+
+    @Override
+    public void init() throws ServletException {
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/views/updateUser.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        String Email=request.getParameter("email");
+        String gender=request.getParameter("gender");
+        Date birthDate = Date.valueOf(request.getParameter("birthDate"));
+
+        User us=new User(id,username,password,Email,gender,birthDate);
+        us.setId(id);
+
+        UserDao userDao=new UserDao();
+        try {
+            int user =userDao.updateUser(con,us);
+            if (user !=0) {
+
+                request.getRequestDispatcher("/login").forward(request, response);
+            } else {
+                System.out.println("你的修改不符合格式");
+            }
+        }
+        catch(SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/webapp/register.jsp
@@ -0,0 +1,26 @@
+<%@include file="WEB-INF/views/header.jsp"%>
+<form  style="text-align:center" method="post" action="register">
+    <!--<input type="text" name="userid" value="" placeholder="登录名"/>-->
+    <h1 style="text-align:center;color:#80002d" >register</h1></br>
+    username:<input type="text" name="username" value="username"
+                    onFocus="if(value==defaultValue){value='';this.style.color='#000'}"
+                    onBlur="if(!value){value=defaultValue;this.style.color='#999'}" style="color:#999999;background: #EAEAAE" /><br/><br/>
+    password:<input type="password" name="password" value="password"
+                    onFocus="if(value==defaultValue){value='';this.style.color='#000'}"
+                    onBlur="if(!value){value=defaultValue;this.style.color='#999'}" style="color:#999999;background: #EAEAAE" /><br/><br/>
+
+    Email:<input type="Email" name="email" value="email"
+                 onFocus="if(value==defaultValue){value='';this.style.color='#000'}"
+                 onBlur="if(!value){value=defaultValue;this.style.color='#999'}" style="color:#999999;background: #EAEAAE" /><br/><br/>
+
+    <input type="radio" name="gender"  id="man1" value="Male" />
+    <label for="man1">Male</label>
+    <input type="radio" name="gender" id="man2" value="Female" />
+    <label for="man2">Female</label><br/><br/>
+    birthDate:<input type="date" name="birthDate" value="birthDate"
+                     onFocus="if(value==defaultValue){value='';this.style.color='#000'}"
+                     onBlur="if(!value){value=defaultValue;this.style.color='#999'}" style="color:#999999;background: #EAEAAE" />
+    <input type="submit" name="Submit" value="Submit" />
+    <input type="submit" name="Submit2" value="reset" />
+</form>
+<%@include file="WEB-INF/views/footer.jsp"%>
